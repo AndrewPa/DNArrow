30dff38e050ce0e3bc10892432ce8ced319494c1
AndrewPa <andrew.papadopoli@gmail.com>
2014-03-31 18:05:11 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,6 @@
- <?php 
-
- $past = time() - 100; 
-
- //this makes the time in the past to destroy the cookie 
-
- setcookie(ID_my_site, gone, $past); 
-
- setcookie(Key_my_site, gone, $past); 
-
- header("Location: index.php"); 
-
- ?> 
+<?php 
+    $past = time() - 100; 
+    setcookie(ID_my_site, gone, $past); 
+    setcookie(Key_my_site, gone, $past);
+    header("Location: index.php"); 
+?>
